1987cff32bbe178de99f0b62de71ed1eae86ece5
zhoucheng <2058856398@qq.com>
2024-05-22 20:27:05 +0800

    同步代码

---
+++ apps/goods/tests.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+import datetime
+
+import json
+
+from django.test import TestCase
+
+# Create your tests here.
+
+"""
+测试
+"""
+# dict_x = {'a':4, 'c':2, 'b':3}
+#
+# print(sorted(dict_x.items(), reverse=True, key=lambda x:x[0]))
+
